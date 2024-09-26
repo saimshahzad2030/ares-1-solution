a51d8c469f87e52dafcc4cc385c4f9e3dd8855e3
Saim Shahzad <saimshehzad2030@gmail.com>
2024-09-26 10:29:45 +0500

    fix:ui fixes

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ import Copyright from "../components/Copyright/Copyright";
 
 export default function Home() {
   return (
-    <div className="bg-white dark:bg-defaultTheme flex flex-col items-center overflow-x-hidden">
+    <div className="bg-white dark:bg-defaultTheme flex flex-col items-center overflow-hidden">
       <Navbar />
       <Hero />
       <AboutSection />
